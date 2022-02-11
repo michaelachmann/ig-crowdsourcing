a356ee09440fc0e323e0a20de5ae9e72c0b751a4
Michael Achmann <michael@achmann.dev>
2022-02-11 16:21:54 +0100

    Messages are working, tailwind included

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,21 +1,37 @@
 <template>
   <div>
-    {{ userIds }}
-    <p>Test: {{ currentStories.audience }}</p>
+    <div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-6xl place-content-center justify-center align-middle">
+      <div class="md:flex">
+        <div class="shrink-0">
+          <img class="w-full object-cover md:h-full md:w-80" :src="currentImage" alt="Man looking at item at a store">
+        </div>
+        <div class="p-8">
+          <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Instagram Story Annotator</div>
+          <p class="mt-2 text-slate-500">Was sehen Sie in der Story? Bitte w&auml;hlen Sie die zutreffenden Optionen aus!</p>
+              <Story v-if="currentStories" class="story" :story="currentStories"/>
+
+        </div>
+      </div>
+    </div>
   </div>
+
+
 </template>
 
 <script>
+import Story from './Story.vue'
+
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  components: {
+    Story
   },
   data() {
     return {
       userIds: [],
-      currentStories: [],
+      currentStories: null,
       currentUser: 0,
+      currentImage: "",
     }
   },
   mounted() {
@@ -24,20 +40,28 @@ export default {
   },
   methods: {
     getUserStories(userId) {
-      chrome.tabs.query({url: "https://www.instagram.com/*"}, results => {
+      chrome.tabs.query({url: "https://www.instagram.com/*"}, function(results) {
       chrome.tabs.sendMessage(results[0].id, {type: 'userStories', userId: userId}, function(response) {
           this.currentStories = response[0]
-          console.log(this.currentStories.audience)
-          })
-      })
+          this.getImage(this.currentStories.display_url)
+          }.bind(this))
+      }.bind(this))
+    },
+    getImage(url){
+      chrome.tabs.query({url: "https://www.instagram.com/*"}, function(results) {
+      chrome.tabs.sendMessage(results[0].id, {type: 'image', url: url}, function(response) {
+          this.currentImage = response
+          console.log(this.currentImage)
+          }.bind(this))
+      }.bind(this))
     },
     getUserIds() {
-      chrome.tabs.query({url: "https://www.instagram.com/*"}, results => {
+      chrome.tabs.query({url: "https://www.instagram.com/*"}, function(results) {
       chrome.tabs.sendMessage(results[0].id, {type: 'userIds'}, function(response) {
           this.userIds = response
           this.currentUser = 0
-          })
-      })
+          }.bind(this))
+      }.bind(this))
     }
   },
   computed: {
@@ -47,18 +71,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.story{
+  white-space: pre-wrap;
 }
 </style>
