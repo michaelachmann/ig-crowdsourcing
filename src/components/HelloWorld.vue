f2d093c56433ea86056f9d57e69aa9e869a2cc44
Michael Achmann <michael@achmann.dev>
2022-02-11 14:14:30 +0100

    First Init for Vue integrated annotator extension

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,7 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    {{ userIds }}
+    <p>Test: {{ currentStories.audience }}</p>
   </div>
 </template>
 
@@ -35,6 +10,37 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data() {
+    return {
+      userIds: [],
+      currentStories: [],
+      currentUser: 0,
+    }
+  },
+  mounted() {
+    this.getUserIds()
+    this.getUserStories(1573431041)
+  },
+  methods: {
+    getUserStories(userId) {
+      chrome.tabs.query({url: "https://www.instagram.com/*"}, results => {
+      chrome.tabs.sendMessage(results[0].id, {type: 'userStories', userId: userId}, function(response) {
+          this.currentStories = response[0]
+          console.log(this.currentStories.audience)
+          })
+      })
+    },
+    getUserIds() {
+      chrome.tabs.query({url: "https://www.instagram.com/*"}, results => {
+      chrome.tabs.sendMessage(results[0].id, {type: 'userIds'}, function(response) {
+          this.userIds = response
+          this.currentUser = 0
+          })
+      })
+    }
+  },
+  computed: {
   }
 }
 </script>
